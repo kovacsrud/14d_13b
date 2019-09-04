0065a40d4f61525e5d4b5561c2b25ef8f348b508
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-04 13:06:56 +0200

    érték típusok
    #1

--- Valtozok/Valtozok/Program.cs
+++ Valtozok/Valtozok/Program.cs
@@ -10,7 +10,35 @@ namespace Valtozok
     {
         static void Main(string[] args)
         {
+            //Érték típusok
+            //szám típusú változók
+            //egész típusok
+            int a = Int32.MaxValue;
+            byte b = Byte.MaxValue;
+            short c = short.MaxValue;
+            long d = 1231;
 
+            //Szöveg 
+
+            String str = "Valami szöveg";
+
+            //karakter
+
+            Char ch = 'z';
+
+            //lebegőpontos számok
+            float e = 1.126f; //32 bites
+            double f = 23.577; //64 bites
+            decimal g = 456.2353455m; //128 bites
+
+            //logikai
+
+            bool igaze = true;
+
+            Console.WriteLine("A:{0}, B:{1}, C:{2}",a,b,c);
+            Console.WriteLine($"A:{a},B:{b},C:{c}");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Valtozok/Valtozok/valtozok.md
@@ -0,0 +1,31 @@
+﻿# Változók, változó típusok
+---
+## Számokat tároló változók
+
+
+### egész típusok
+```c#
+            int a = 12;
+            byte b = 125;
+            short c = -12;
+            long d = 1231;
+```
+
+### Szöveg 
+
+            String str = "Valami szöveg";
+
+### karakter
+
+            Char ch = 'z';
+
+### lebegőpontos számok
+            float e = 1.126f; //32 bites
+            double f = 23.577; //64 bites
+            decimal g = 456.2353455m; //128 bites
+
+### logikai
+
+            bool igaze = true;
+
+Ezek a változók ún. érték típusú változók
