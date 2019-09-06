becc94d8e3b671cf11824e2527bf059cbe576bda
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-06 08:04:33 +0200

    String gyakorlófeladat

---
+++ StringGyakorlas/StringGyakorlas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringGyakorlas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg a mondatot:");
+            //A Console.ReadLine mindig String-be olvas be
+            var mondat = Console.ReadLine();
+
+            Console.WriteLine("Adja meg a szót:");
+            var szo = Console.ReadLine();
+
+            if (mondat.Contains(szo))
+            {
+                Console.WriteLine("Tartalmazza");
+            } else
+            {
+                Console.WriteLine("Nem tartalmazza");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
