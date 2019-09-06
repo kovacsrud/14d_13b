c1117cedad322f2532fc39064adb2eeee18dbcf3
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-06 10:36:58 +0200

    Lotto start

---
+++ LottoGame/LottoGame/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LottoGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hány számmal játszunk?:");
+            var szamDb = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Mennyiből sorsolunk?:");
+            var osszSzam = Convert.ToInt32(Console.ReadLine());
+
+            int[] tippek = new int[szamDb];
+            int[] nyeroSzamok = new int[osszSzam];
+
+            //Bekérés
+            //Ugyanaz a szám nem szerepelhet
+            //1-nél kisebb nem lehet
+            //osszSzam-nal nagyobb nem lehet
+
+
+            //Nyerőszámok sorsolása
+
+        }
+    }
+}
