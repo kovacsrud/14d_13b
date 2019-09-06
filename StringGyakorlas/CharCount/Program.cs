57e52e8bb2f6b33aa4ff9837ff57e61a751276f6
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-06 08:18:57 +0200

    Karakter számlálás

---
+++ StringGyakorlas/CharCount/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Adja meg a mondatot:");
+            var mondat = Console.ReadLine();
+            Console.WriteLine("Adjon meg egy karaktert:");
+            var karakter = Console.ReadKey().KeyChar;
+            var szamlalo = 0;
+            Console.WriteLine();
+
+            for (int i = 0; i < mondat.Length; i++)
+            {
+                if (mondat[i]==karakter)
+                {
+                    szamlalo++;
+                }
+            }
+
+            Console.WriteLine($"{karakter} karakterből {szamlalo} db van.");
+            Console.ReadKey();
+        }
+    }
+}
