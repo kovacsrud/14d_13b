0983c4f2fb0f5cd1da38522039a968440908cbc7
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-16 11:45:04 +0200

    OOp alapok, getter, setter, példányosítás

---
+++ OOP/OOP0916/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP0916
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //az UjEmber osztály példányosítása
+            UjEmber zeno = new UjEmber();
+            zeno.setNev("Zabos Zénó");
+
+            Console.WriteLine(zeno.getNev());
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ OOP/OOP0916/UjEmber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP0916
+{
+    class UjEmber
+    {
+        //az osztály adattagjai
+        private String nev;
+        private int kor;
+        private String neme;
+        private int suly;
+
+        //a név lekérdezése
+        public string getNev()
+        {
+            return nev;
+        }
+        public void setNev(string pnev)
+        {
+            nev = pnev;
+        }
+
+
+    }
+}
