1e62b550746c1c9b8ec7adceb23dd56eadb12372
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-09-24 13:03:35 +0200

    Fájlok és listák

---
+++ Fajlok/Fajlok/Ember.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fajlok
+{
+    class Ember
+    {
+        public string Vezeteknev { get; set; }
+        public string Keresztnev { get; set; }
+        public int SzuletesEve { get; set; }
+        public string Szuletesihely { get; set; }
+
+    }
+}
---
+++ Fajlok/Fajlok/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Fajlok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. megoldás
+            //try
+            //{
+            //    FileStream fajl = new FileStream(@"d:/rud/tesztadat_20k.txt", FileMode.Open);
+            //    StreamReader sr = new StreamReader(fajl, Encoding.Default);
+
+            //    while (!sr.EndOfStream)
+            //    {
+            //        Console.WriteLine(sr.ReadLine());
+            //    }
+
+            //    sr.Close();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+
+            //2. megoldás, fájl zárása a finally block-ban
+
+            //FileStream fajl = null;
+            //StreamReader sr = null;
+
+            //try
+            //{
+            //    fajl = new FileStream(@"d:/rud/tesztadat_20k.txt", FileMode.Open);
+            //    sr = new StreamReader(fajl, Encoding.Default);
+
+            //    while (!sr.EndOfStream)
+            //    {
+            //        Console.WriteLine(sr.ReadLine());
+            //    }
+
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);                
+            //}
+            //finally
+            //{
+            //    //Amit itt van az minden esetben végrehajtódik
+            //    //Logikus, hogy itt zárjuk be a fájlt
+            //    if (sr!=null)
+            //    {
+            //        sr.Close();
+            //    }
+
+
+            //}
+
+            //3. megoldás
+
+            List<Ember> emberek = new List<Ember>();
+            
+            try
+            {
+                FileStream fajl = new FileStream(@"d:/rud/tesztadat_20k.txt", FileMode.Open);
+
+                using (StreamReader sr = new StreamReader(fajl, Encoding.Default))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        var elemek=sr.ReadLine().Split(',');
+                        Ember ember = new Ember
+                        {
+                            Vezeteknev = elemek[0],
+                            Keresztnev = elemek[1],
+                            SzuletesEve = Convert.ToInt32(elemek[2]),
+                            Szuletesihely = elemek[3]
+
+                        };
+                        emberek.Add(ember);
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.WriteLine(emberek.Count);
+
+            var al = emberek.FindAll(x=>x.Vezeteknev.ToLower().StartsWith("al")).OrderBy(x=>x.Vezeteknev).ThenBy(x=>x.Keresztnev);
+
+            Console.WriteLine(al.Count());
+
+            foreach (var n in al)
+            {
+                Console.WriteLine($"{n.Vezeteknev},{n.Keresztnev},{n.SzuletesEve},{n.Szuletesihely}");
+            }
+
+            var alstat = al.ToLookup(x=>new { x.Vezeteknev,x.Keresztnev });
+
+            foreach (var s in alstat)
+            {
+                Console.WriteLine($"{s.Key.Vezeteknev},{s.Key.Keresztnev},{s.Count()}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
