0ec6731fc261adc400f7277df21b3a0cdcec976c
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-10-02 12:04:25 +0200

    Kivételek, using minta

---
+++ Kivetelek/Kivetelek/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kivetelek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                throw new ArithmeticException();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(NullReferenceException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.Source);
+                Console.WriteLine(ex.StackTrace);
+                
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Kivetelek/Memoria/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace Memoria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (!Console.KeyAvailable)
+            {
+                using (var bitmap = new Bitmap(1280, 1024))
+                {
+                    Console.WriteLine(GC.GetTotalMemory(false));
+                }
+
+                
+                
+            }
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
