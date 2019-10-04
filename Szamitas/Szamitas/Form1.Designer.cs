0075e40b920b910317cc50c88946bbb86a08f70e
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-10-04 09:45:45 +0200

    Egyszerű számoló program

---
+++ Szamitas/Szamitas/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Szamitas
+{
+    partial class formSzamolo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelEredmeny = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.buttonSzamol = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(292, 63);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "D=(A+B)/C";
+            // 
+            // labelEredmeny
+            // 
+            this.labelEredmeny.AutoSize = true;
+            this.labelEredmeny.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.labelEredmeny.Location = new System.Drawing.Point(23, 121);
+            this.labelEredmeny.Name = "labelEredmeny";
+            this.labelEredmeny.Size = new System.Drawing.Size(0, 63);
+            this.labelEredmeny.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(355, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 46);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "A:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(355, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 46);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "B:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(355, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 46);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "C:";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxA.Location = new System.Drawing.Point(434, 26);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(277, 38);
+            this.textBoxA.TabIndex = 5;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxB.Location = new System.Drawing.Point(434, 121);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(277, 38);
+            this.textBoxB.TabIndex = 6;
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBoxC.Location = new System.Drawing.Point(434, 220);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(277, 38);
+            this.textBoxC.TabIndex = 7;
+            // 
+            // buttonSzamol
+            // 
+            this.buttonSzamol.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.buttonSzamol.Location = new System.Drawing.Point(513, 299);
+            this.buttonSzamol.Name = "buttonSzamol";
+            this.buttonSzamol.Size = new System.Drawing.Size(117, 36);
+            this.buttonSzamol.TabIndex = 8;
+            this.buttonSzamol.Text = "Számolás";
+            this.buttonSzamol.UseVisualStyleBackColor = true;
+            this.buttonSzamol.Click += new System.EventHandler(this.buttonSzamol_Click);
+            // 
+            // formSzamolo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 362);
+            this.Controls.Add(this.buttonSzamol);
+            this.Controls.Add(this.textBoxC);
+            this.Controls.Add(this.textBoxB);
+            this.Controls.Add(this.textBoxA);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelEredmeny);
+            this.Controls.Add(this.label1);
+            this.Name = "formSzamolo";
+            this.Text = "Számolás";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelEredmeny;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.Button buttonSzamol;
+    }
+}
+
---
+++ Szamitas/Szamitas/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Szamitas
+{
+    public partial class formSzamolo : Form
+    {
+        public formSzamolo()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSzamol_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var d = (Convert.ToInt32(textBoxA.Text) + Convert.ToInt32(textBoxB.Text)) / Convert.ToInt32(textBoxC.Text);
+                labelEredmeny.Text = Convert.ToString(d);
+                
+            }
+            catch (DivideByZeroException ex)
+            {
+                MessageBox.Show("0-val való osztás hiba!","Hiba!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("A mezőkbe számot kell megadni!","Hiba!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Általános hiba",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            }
+            
+        }
+    }
+}
