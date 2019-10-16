ef6573717e2b3c403b46aa3d76d60bf0db15d416
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-10-16 12:38:40 +0200

    Időjárás adatkezelő

---
+++ WpfIdojaras/WpfIdojaras/IdoJarasAdat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfIdojaras
+{
+    public class IdoJarasAdat
+    {
+       
+
+        public int Ev { get; set; }
+        public int Honap { get; set; }
+        public int Nap { get; set; }
+        public int Ora { get; set; }
+        public double Homerseklet { get; set; }
+        public double Szelsebesseg { get; set; }
+        public double Paratartalom { get; set; }
+
+        public IdoJarasAdat(int ev, int honap, int nap, int ora, double homerseklet, double szelsebesseg, double paratartalom)
+        {
+            Ev = ev;
+            Honap = honap;
+            Nap = nap;
+            Ora = ora;
+            Homerseklet = homerseklet;
+            Szelsebesseg = szelsebesseg;
+            Paratartalom = paratartalom;
+        }
+
+    }
+}
---
+++ WpfIdojaras/WpfIdojaras/IdoJarasAdatok.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfIdojaras
+{
+    public class IdoJarasAdatok
+    {
+        private string filePath;
+        private List<IdoJarasAdat> adatlista = new List<IdoJarasAdat>();
+
+        public IdoJarasAdatok(string filepath)
+        {
+            //elérési út beállítása
+            filePath = filepath;
+            fileLoad();
+
+        }
+
+        private void fileLoad()
+        {
+            try
+            {
+                var sorok = File.ReadAllLines(filePath,Encoding.Default);
+
+                for (int i = 1; i < sorok.Length; i++)
+                {
+                    var e = sorok[i].Split(';');
+                    IdoJarasAdat adat = new IdoJarasAdat(
+                            Convert.ToInt32(e[0]),
+                            Convert.ToInt32(e[1]),
+                            Convert.ToInt32(e[2]),
+                            Convert.ToInt32(e[3]),
+                            Convert.ToDouble(e[4]),
+                            Convert.ToDouble(e[5]),
+                            Convert.ToDouble(e[6])
+                        );
+
+                    adatlista.Add(adat);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Hiba!",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+        }
+
+        public int GetLength()
+        {
+            return adatlista.Count;
+        }
+    }
+}
