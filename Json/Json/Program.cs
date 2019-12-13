b245cfaec3fc1233777278bd364ce5e16193c0a4
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2019-12-13 07:57:31 +0100

    Json fájlok kezelése
    Json fájlok kezelése

---
+++ Json/Json/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String json = "";
+
+            try
+            {
+                json = File.ReadAllText(@"colors.json", Encoding.Default);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                
+            }
+            Console.WriteLine(json);
+
+            JsonTextReader reader = new JsonTextReader(new StringReader(json));
+
+            //while (reader.Read())
+            //{
+            //    Console.WriteLine($"{reader.TokenType},{reader.ValueType},{reader.Value}");
+            //}
+
+            JObject colors = JObject.Parse(json);
+
+            var jsColors = colors.SelectToken("colors");
+
+            Console.WriteLine(jsColors[0]["code"]["rgba"]);
+
+            var codes = colors.SelectTokens("$..code");
+
+            var reds = colors.SelectTokens("$.colors[?(@.color=='red')]");
+
+            Console.WriteLine(codes.Count());
+            Console.WriteLine(reds.Count());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Json/WpfIP/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfIP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        String apiKey = "57e7f2daa88da7119dc6c575f1232c0f";
+        JObject ipData;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            GetIPData("2.16.7.2");
+            Debug.WriteLine(ipData);
+            Debug.WriteLine(ipData["city"]);
+            stackData.Children.Add(DataToLabel((string)ipData["continent_name"]));
+            stackData.Children.Add(DataToLabel((string)ipData["country_name"]));
+            stackData.Children.Add(DataToLabel((string)ipData["city"]));
+        }
+
+        public void GetIPData(string ip)
+        {
+            ipData = JObject.Parse(new WebClient().DownloadString($"http://api.ipstack.com/{ip}?access_key={apiKey}"));
+        }
+
+        public Label DataToLabel(string data)
+        {
+            Label label = new Label();
+            label.Content = data;
+            return label;
+        }
+
+    }
+}
