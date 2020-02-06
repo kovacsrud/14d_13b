3bea9b4a03f02491f58c350986d06af48dd9f243
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-02-06 08:25:47 +0100

    KepDb

---
+++ KepDb/KepDb/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KepDb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InsertGumi();
+            ReadKep();
+        }
+        public void InsertGumi()
+        {
+            byte[] kepfilebytes = File.ReadAllBytes(@"michelin.jpg");
+
+            string connString = "Data Source=gumiabroncs.db;Version=3";
+            SQLiteConnection conn = new SQLiteConnection(connString);
+            conn.Open();
+            SQLiteCommand comm = new SQLiteCommand(conn);
+            comm.CommandText = "INSERT INTO abroncsok (marka,meret,gumikep) "+
+                "values (@marka,@meret,@gumikep)";
+
+            comm.Parameters.Add("@marka", DbType.String, 8).Value = "Michelin";
+            comm.Parameters.Add("@meret", DbType.String, 8).Value = "165*70*R14";
+            comm.Parameters.Add("@gumikep", DbType.Binary, kepfilebytes.Length).Value = kepfilebytes;
+
+            var sor=comm.ExecuteNonQuery();
+            Debug.WriteLine(sor);
+
+
+            conn.Close();
+
+        }
+
+        public void ReadKep()
+        {
+            string connString = "Data Source=gumiabroncs.db;Version=3";
+            SQLiteConnection conn = new SQLiteConnection(connString);
+            conn.Open();
+            SQLiteCommand comm = new SQLiteCommand(conn);
+            comm.CommandText = "select gumikep from abroncsok where Id=1";
+            byte[] gumipic = (byte [])comm.ExecuteScalar();
+
+            MemoryStream ms = new MemoryStream(gumipic);
+            BitmapImage gumibitmap = new BitmapImage();
+            gumibitmap.BeginInit();
+            gumibitmap.StreamSource = ms;
+            gumibitmap.EndInit();
+
+            gumikep.Source = gumibitmap;
+
+            conn.Close();
+        }
+
+
+
+    }
+}
---
+++ WpfAdatkotes/WpfAdatkotes/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAdatkotes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window,INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            racs.DataContext = this;
+            this.Probaszoveg = "";
+        }
+
+        private string probaszoveg;
+        public String Probaszoveg {
+            get { return probaszoveg; }
+            set
+            {
+                probaszoveg = value;
+                if (null!=this.PropertyChanged)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Probaszoveg"));
+                }
+            }
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+    }
+}
