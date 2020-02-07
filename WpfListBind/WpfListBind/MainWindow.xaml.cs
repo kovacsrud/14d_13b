488594745905e73d6f16dd4cdde41813f18f306d
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-02-07 10:08:21 +0100

    Összetett elemek adatkötéssel

---
+++ WpfListBind/WpfListBind/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfListBind
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Szemely> szemelyek = new List<Szemely>();
+            szemelyek.Add(new Szemely("3-191919-1234","Kiss","Anna", "p1.jpg"));
+            szemelyek.Add(new Szemely("3-181818-4321", "Veress", "Eufrozina", "p2.jpg"));
+            szemelyek.Add(new Szemely("3-201819-4322", "Korompai", "Ágnes", "p3.jpg"));
+
+            DataContext = szemelyek;
+        }
+    }
+}
---
+++ WpfListBind/WpfListBind/Szemely.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace WpfListBind
+{
+    public class Szemely
+    {
+        public Szemely(string szemelyiSzam, string vezeteknev, string keresztnev, string kepfajl)
+        {
+            SzemelyiSzam = szemelyiSzam;
+            Vezeteknev = vezeteknev;
+            Keresztnev = keresztnev;
+            try
+            {
+                byte[] kepadatok = File.ReadAllBytes(kepfajl);
+                MemoryStream ms = new MemoryStream(kepadatok);
+                BitmapImage szemelyfoto = new BitmapImage();
+                szemelyfoto.BeginInit();
+                szemelyfoto.StreamSource = ms;
+                szemelyfoto.EndInit();
+                Szemelyfoto = szemelyfoto;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);               
+            }
+
+            
+        }
+
+        public string SzemelyiSzam { get; set; }
+        public string Vezeteknev { get; set; }
+        public string Keresztnev { get; set; }
+        public BitmapImage Szemelyfoto { get; set; }
+
+    }
+
+
+}
