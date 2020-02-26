d4a4f141a8af17d4f44ec33cc5a6f3bd3b86760c
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-02-26 11:32:25 +0100

    Unit teszt bevezető

---
+++ AlapMuveletek/AlapMuveletek/Alapmuvelet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlapMuveletek
+{
+    public class Alapmuvelet
+    {
+        public double Osszead(double a,double b)
+        {
+            return a + b;
+        }
+
+        public double Kivon(double a,double b)
+        {
+            return a - b;
+        }
+
+        public double Szoroz(double a,double b)
+        {
+            return a * b;
+        }
+
+        public double Oszt(double a,double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ AlapMuveletek/Alapmuveletek.Test/AlapTest.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlapMuveletek;
+
+namespace Alapmuveletek.Test
+{
+    [TestClass]
+    public class AlapTest
+    {
+        [TestMethod]
+        public void OsszeadTest()
+        {
+            //Arrange
+            //előkészületek pl. példányosítás
+            Alapmuvelet alap = new Alapmuvelet();
+            //Act
+            //valamilyen tevékenység elvégzése 
+            //metódus hívás 
+
+            //Assert
+            //Tesztelés, adott típusú-e, jó eredményt ad-e stb.
+            Assert.IsNotNull(alap.Osszead(5, 5));
+            Assert.AreEqual(20, alap.Osszead(10, 10));
+
+        }
+
+        [TestMethod]
+        public void Kivonteszt()
+        {
+            var alap = new Alapmuvelet();
+            Assert.IsNotNull(alap.Kivon(6,6));
+            Assert.AreEqual(5, alap.Kivon(10, 5));
+            
+        }
+
+        [TestMethod]
+        public void Szorzasteszt()
+        {
+            var alap = new Alapmuvelet();
+            Assert.IsNotNull(alap.Szoroz(6, 6));
+            Assert.AreEqual(50, alap.Szoroz(10, 5));
+
+        }
+
+        [TestMethod]
+        public void Osztasteszt()
+        {
+            var alap = new Alapmuvelet();
+            Assert.IsNotNull(alap.Oszt(6, 6));
+            Assert.AreEqual(2, alap.Oszt(10, 5));
+            
+
+        }
+    }
+}
