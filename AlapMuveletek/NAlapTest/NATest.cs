6d6169b734621370e7d72cb9e711aa94a0a0aa99
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-02-26 13:02:17 +0100

    Tesztesetek definiálása, tolerancia (delta paraméter) megadása

--- AlapMuveletek/NAlapTest/NATest.cs
+++ AlapMuveletek/NAlapTest/NATest.cs
@@ -8,13 +8,16 @@ namespace NAlapTest
     public class NATest
     {
         [Test]
-        public void TestOsszead()
+        [TestCase(10,10,20)]
+        [TestCase(10,20,30)]
+        [TestCase(30.5,30.48,61)]
+        public void TestOsszead(double a,double b,double elvart)
         {
             
             var sut = new Alapmuvelet();
 
             Assert.IsNotNull(sut);
-            Assert.AreEqual(10,sut.Osszead(5,5));
+            Assert.AreEqual(elvart,sut.Osszead(a,b),0.03);
         }
     }
 }
