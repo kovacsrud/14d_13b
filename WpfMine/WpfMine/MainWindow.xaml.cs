ecbbbd421694932e2dec2936a062678696aad286
kovacsrud <42613777+kovacsrud@users.noreply.github.com>
2020-03-06 10:16:32 +0100

    FontAwesome bemutató

---
+++ WpfMine/WpfMine/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfMine
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MineItems mineItems = new MineItems();
+            mineField.Children.Add(mineItems.num1);
+            Grid.SetColumn(mineItems.num1, 3);
+            mineField.Children.Add(mineItems.flag);
+            Grid.SetColumn(mineItems.flag, 0);
+            Grid.SetRow(mineItems.flag, 1);
+        }
+    }
+}
---
+++ WpfMine/WpfMine/MineItems.cs
@@ -0,0 +1,43 @@
+﻿using FontAwesome.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace WpfMine
+{
+    public class MineItems
+    {
+        public ImageAwesome bomb=new ImageAwesome();
+        public ImageAwesome flag = new ImageAwesome();
+        public FontAwesome.WPF.FontAwesome num1 = new FontAwesome.WPF.FontAwesome();
+        public FontAwesome.WPF.FontAwesome num2 = new FontAwesome.WPF.FontAwesome();
+        public FontAwesome.WPF.FontAwesome num3 = new FontAwesome.WPF.FontAwesome();
+        public FontAwesome.WPF.FontAwesome num4 = new FontAwesome.WPF.FontAwesome();
+        public FontAwesome.WPF.FontAwesome num5 = new FontAwesome.WPF.FontAwesome();
+        public FontAwesome.WPF.FontAwesome num6 = new FontAwesome.WPF.FontAwesome();
+        public FontAwesome.WPF.FontAwesome num7 = new FontAwesome.WPF.FontAwesome();
+        public FontAwesome.WPF.FontAwesome num8 = new FontAwesome.WPF.FontAwesome();
+
+        public MineItems()
+        {
+            bomb.Icon = FontAwesomeIcon.Bomb;
+            flag.Icon = FontAwesomeIcon.Flag;
+            num1.Text = "1";
+            num1.FontSize = 50;
+            num1.Foreground = Brushes.Blue;
+            num2.Text = "2";
+            num3.Text = "3";
+            num4.Text = "4";
+            num5.Text = "5";
+            num6.Text = "6";
+            num7.Text = "7";
+            num8.Text = "8";
+        }
+    }
+
+    
+
+}
